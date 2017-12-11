840cccf04ec72b49afd7bb7332d68c2c6bfc8e8f
ananantn <nortonanthonya@gmail.com>
2017-12-11 15:07:45 -0800

    fix bug loading node_modules js

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,51 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 
-// Compile Sass & Inject Into Browser
+function isDefined(item, name) {
+  if (item == null) {
+    console.warn(`${name} is not defined!`);
+  } else {
+    console.log(`${name} is defined.`);
+  }
+};
+
+[
+  [null, 'null'],
+  [undefined, 'undefined'],
+  [gulp, 'gulp'],
+  [browserSync, 'browserSync'],
+  [sass, 'sass'],
+].forEach(function (item) { isDefined(item[0], item[1]); });
+
 gulp.task('sass', function () {
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-  .pipe(sass())
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
+  return gulp.src(['node_modules/bootstrap-beta/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
 gulp.task('js', function () {
-  return gulp.src([
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/umd/popper.min.js',
-  ])
-  .pipe(gulp.dest('src/js'))
-  .pipe(browserSync.stream());
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js','node_modules/bootstrap-beta/dist/js/bootstrap.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream());
 });
 
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function () {
-  browserSync.init({
-    server: './src',
-  });
+gulp.task('serve', function () {
+  browserSync.init({server: './src'});
 
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
 });
 
-// Move Fonts Folder to src
 gulp.task('fonts', function () {
   return gulp.src('node_modules/font-awesome/fonts/*')
-  .pipe(gulp.dest('src/fonts'));
+    .pipe(gulp.dest('src/fonts'));
 });
 
-
-// Move Fonts Awesome CSS to src/css
 gulp.task('fa', function () {
   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
     .pipe(gulp.dest('src/css'));
 });
 
-
-gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
+gulp.task('default', ['sass', 'js', 'serve', 'fonts', 'fa']);
